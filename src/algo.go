549ee91b5027fb4c50f7b20e8f491fd68d73d2bb
Marco <marco.pellero.it@gmail.com>
2023-01-24 07:03:11 +0100

    initial code; bugged!

--- src/algo.go
+++ src/algo.go
@@ -1,1 +1,149 @@
 package main
+
+import (
+	"bytes"
+	"math"
+	"strings"
+)
+
+type Guess struct {
+	word    string
+	entropy float64
+}
+
+func is_candidate(word, guess string, pattern []byte) bool {
+	letter_counter := make([]int, 26)
+
+	for i := 0; i < len(word); i++ {
+		if pattern[i] != 'g' {
+			continue
+		} else if guess[i] != word[i] {
+			return false
+		}
+
+		letter_counter[guess[i]-'a']++
+	}
+
+	for i := 0; i < len(word); i++ {
+		if pattern[i] != 'y' {
+			continue
+		} else if guess[i] == word[i] {
+			return false
+		}
+
+		if strings.Count(word, string(guess[i])) <= letter_counter[guess[i]-'a'] {
+			return false
+		}
+		letter_counter[guess[i]-'a']++
+	}
+
+	for i := 0; i < len(word); i++ {
+		if pattern[i] != 'b' {
+			continue
+		} else if guess[i] == word[i] {
+			return false
+		}
+
+		for j := i + 1; j < len(word); j++ {
+			if pattern[j] == 'y' && guess[i] == guess[j] {
+				return false
+			}
+		}
+
+		if strings.Count(word, string(guess[i])) > letter_counter[guess[i]-'a'] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func get_candidates(wordlist []string, guess string, pattern []byte) []string {
+	candidates := make([]string, 0)
+
+	for _, word := range wordlist {
+		if is_candidate(word, guess, pattern) {
+			candidates = append(candidates, word)
+		}
+	}
+
+	return candidates
+}
+
+func calculate_pattern_entropy(wordlist []string, guess string, pattern []byte) float64 {
+	num_of_candidates := 0
+	for _, word := range wordlist {
+		if is_candidate(word, guess, pattern) {
+			num_of_candidates++
+		}
+	}
+
+	if num_of_candidates == 0 {
+		return 0
+	}
+
+	px := float64(num_of_candidates) / float64(len(wordlist))
+	return -px * math.Log2(px)
+}
+
+func calculate_guess_entropy(wordlist []string, guess string) float64 {
+	entropy := .0
+
+	color_counter := []int{0, 0} // [G, Y]
+
+	pattern := bytes.Repeat([]byte{'b'}, len(guess))
+	for {
+		// if all letters are green, except for a yellow one, the pattern is illegal and should not be considered
+		if color_counter[0] != len(guess)-1 || color_counter[1] != 1 {
+			entropy += calculate_pattern_entropy(wordlist, guess, pattern)
+		}
+
+		// go to next pattern
+		is_last := false
+		for i := 0; i <= len(pattern); i++ {
+			if i == len(pattern) {
+				is_last = true
+				break
+			}
+
+			if pattern[i] == 'b' {
+				pattern[i] = 'y'
+				color_counter[1]++
+			} else if pattern[i] == 'y' {
+				pattern[i] = 'g'
+				color_counter[1]--
+				color_counter[0]++
+			} else {
+				color_counter[0]--
+				pattern[i] = 'b'
+				continue
+			}
+			break
+		}
+
+		if is_last {
+			break
+		}
+	}
+
+	return entropy
+}
+
+func get_optimal_guess(candidates, wordlist []string) Guess {
+	if len(candidates) == 1 {
+		return Guess{candidates[0], 0}
+	}
+
+	best := Guess{candidates[0], -1}
+	for _, word := range wordlist {
+		entropy := calculate_guess_entropy(candidates, word)
+		// if word == "cedro" {
+		// 	fmt.Printf("\t\t%s %f\n", word, entropy)
+		// }
+		if entropy > best.entropy {
+			best = Guess{word, entropy}
+		}
+	}
+
+	return best
+}
--- src/simulations.go
+++ src/simulations.go
@@ -1,1 +1,81 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func get_pattern(guess, hidden string) []byte {
+	pattern := bytes.Repeat([]byte{0}, len(hidden))
+	green_counter := make([]int, 26)
+
+	for i := 0; i < len(hidden); i++ {
+		if guess[i] == hidden[i] {
+			pattern[i] = 'g'
+			green_counter[guess[i]-'a']++
+		} else if !strings.Contains(hidden, string(guess[i])) {
+			pattern[i] = 'b'
+		}
+	}
+
+	for i := 0; i < len(hidden); i++ {
+		if pattern[i] != 0 {
+			continue
+		}
+
+		if green_counter[guess[i]-'a'] != strings.Count(hidden, string(guess[i])) {
+			pattern[i] = 'y'
+			green_counter[guess[i]-'a']++
+		} else {
+			pattern[i] = 'b'
+		}
+	}
+
+	return pattern
+}
+
+func read_wordlist(path string) []string {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't open wordlist file at [%s]", path))
+	}
+
+	return strings.Split(string(data), "\n")
+}
+
+func play_single_word(wordlist []string, first_guess, hidden string) int {
+	guesses := 1
+	pattern := get_pattern(first_guess, hidden)
+	candidates := get_candidates(wordlist, first_guess, pattern)
+
+	for !bytes.Equal(pattern, bytes.Repeat([]byte{'g'}, len(pattern))) {
+		guesses++
+		next_guess := get_optimal_guess(candidates, wordlist)
+		pattern = get_pattern(next_guess.word, hidden)
+
+		fmt.Printf("\t[%d] [%s %f] %s\n", len(candidates), next_guess.word, next_guess.entropy, pattern)
+		candidates = get_candidates(candidates, next_guess.word, pattern)
+	}
+
+	return guesses
+}
+
+func play_dictionary(path, first_guess string) float64 {
+	mean := .0
+	total_guesses := 0
+
+	wordlist := read_wordlist(path)
+	for i, word := range wordlist {
+		fmt.Printf("Solving for %s\n", word)
+
+		guesses := play_single_word(wordlist, first_guess, word)
+		total_guesses += guesses
+		mean = float64(total_guesses) / float64(i+1)
+
+		fmt.Printf("[%d / %d] [%f]  %s: %d\n\n", i, len(wordlist), mean, word, guesses)
+	}
+
+	return mean
+}
